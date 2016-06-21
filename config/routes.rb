23998f5542a46711b9f62cf67b8b73fa473e985a
antblessing <antblessing@gmail.com>
2016-06-20 21:32:17 -0700

    Stocks and stocks search form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   get 'welcome/index'
 
   get 'welcome/about'
 
   root 'welcome#index'
+
+  get 'my_portfolio', to: "users#my_portfolio"
+
+  get 'search_stocks', to: "stocks#search"
 end
